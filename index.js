241daffab4fdb0cbf2753d0d06625c33c0576a25
ehasanulh1 <ehasanulh1@gmail.com>
2023-03-11 03:00:49 +0600

    basic code

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('Hello from genius car server')
+});
+
+app.listen(port, () => {
+    console.log(`Listening to port ${port}`)
+})
